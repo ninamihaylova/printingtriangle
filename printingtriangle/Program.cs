859d04f07e27fe18efe590cbf1b8720293ddbcc2
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 14:28:38 +0300

    Initial commit

---
+++ printingtriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            PrintTriangle(num);
+        }
+ 
+        static void  PrintRow (int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+ 
+            Console.WriteLine();
+        }
+        static void PrintTriangle(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+               PrintRow(1, row);
+            }
+            for (int row = n - 1;  row >= 1; row--)
+            {
+                PrintRow(1, row);
+            }
+        }
+    }
